e1327135f1126a0bf5725dcfba487e19190fd7e8
ZAKARIARD2 <59122759+ZAKARIARD2@users.noreply.github.com>
2020-09-06 14:05:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,15 +34,15 @@ client.on("ready", () => {
 
 
    client.on('guildMemberAdd', user => {
-        let joinEmbed = new Discord.RichEmbed()
-              
-               var embed = new Discord.RichEmbed()
-              .setColor('#ffffff')
-              .setAuthor(user.user.username, user.user.displayAvatarURL)
-              .setThumbnail(user.user.displayAvatarURL)
-              .setDescription("**WELCOME" + user + "To Official Server Of Team Vare Make sure to read the <#746395598894268547 >**"  )     
-             .setImage("https://cdn.discordapp.com/attachments/751538929651417178/751552594966741033/WELCOME_DISCORD.jpg")
-             .setFooter('Coded by zakaria');
-             user.guild.channels.get("750349078445031533").send(joinEmbed)
-            });
+  let joinEmbed = new Discord.RichEmbed()
+        
+         var embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setAuthor(user.user.username, user.user.displayAvatarURL)
+        .setThumbnail(user.user.displayAvatarURL)
+        .setDescription("**WELCOME**" + user + "**To Official Server Of Team Vare Make sure to read the <#746395598894268547 >**"  )     
+       .setImage("https://cdn.discordapp.com/attachments/751538929651417178/751552594966741033/WELCOME_DISCORD.jpg")
+       .setFooter('Coded by zakaria');
+       user.guild.channels.get("750349078445031533").send(joinEmbed)
+      });
 client.login(process.env.TOKEN);
