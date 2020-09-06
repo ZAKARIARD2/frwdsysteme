1bb712f5170ea7c10c0398b1d0997b403d50fd33
ZAKARIARD2 <59122759+ZAKARIARD2@users.noreply.github.com>
2020-09-06 14:24:32 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,42 @@
-const { Client, RichEmbed } = require("discord.js");
-var { Util } = require('discord.js');
-const client = new Client({ disableEveryone: true})
 const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("guildMemberAdd", function(user) {
+  let embed = new Discord.RichEmbed()
+    .setTitle("Welcome to ")
+    .setThumbnail()
+
+    .setDescription(`
+      
+**Thank you for joining us! Enjoy your stay, and have a wonderful time!**
 
 
+Please check out...
 
- 
+♪ - <#746395598894268547> 
 
-//status
-var used1 = false;
-
-var version = "1.0.0";
-client.on("ready", () => {
-  console.log("Ch3ltini hh" + version);
-  client.user.setStatus("idle").catch(console.error);
-  setInterval(() => {
-    if (used1) {
-      client.user.setActivity("Coded by zakaria.", {
-        type: "playing"
-      });
-      used1 = false;
-    } else {
-      client.user.setActivity(`#VAREONTOP`, {
-        type: "watching"
-      });
-      used1 = true;
-    }
-  }, 5000);
+To get yourself started !`)
+    .setImage("https://cdn.discordapp.com/attachments/405817866399776768/701564502394536005/welcome.jpg")
+    .setFooter("https://cdn.discordapp.com/attachments/405817866399776768/701564502394536005/welcome.jpg","Coded by zakaria")
+    .setThumbnail(client.user.displayAvatarURL
+);
+  user.guild.channels.get("750349078445031533").send(embed);
+  
 });
 
-
-
-
-
-      client.on("guildMemberAdd", user =>{
-        let joinEmbed = new Discord.RichEmbed()
-        .setColor("#0099ff")
-      .setImage("https://cdn.discordapp.com/attachments/751538929651417178/751552594966741033/WELCOME_DISCORD.jpg")
-      .setAuthor(user.user.username, user.user.displayAvatarURL)
-      .setDescription("**WELCOME**" + user + "**To Official Server Of Team Vare Make sure to read the <#746395598894268547 >**"  )     
-      .setFooter('Coded by zakaria');
-      user.guild.channels.get("750349078445031533").send(joinEmbed)
-      });
+client.on("ready", () => {
+  const channel = client.channels.get("750349078445031533");
+  if (!channel) return console.error("The channel does not exist!");
+  channel
+    .join()
+    .then(connection => {
+      // ila tl3o lk f console hd ktba 3rf 5dam
+      console.log("Successfully connected.");
+    })
+    .catch(e => {
+      // error
+      console.error(e);
+      return;
+    });
+});
 client.login(process.env.TOKEN);
